5e152743075c882a7f5b6254811c24d9a6bc7e45
Yifan Qiao <yifanqiao@berkeley.edu>
2025-09-05 23:57:01 -0700

    chore [setup]: update setup scripts for vllm and diffusion, add new .gitignore entries, and remove obsolete vllm-patch file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -23,7 +23,55 @@ setup_uv_venv() {
     echo "uv venv setup complete"
 }
 
+setup_vllm() {
+    echo "Setting up vllm..."
+    if [ ! -d $GVM_DIR/app/vllm-v0.10.1 ]; then
+        source $GVM_DIR/.venv/bin/activate
+        pushd $GVM_DIR/app/
+        git clone -b v0.10.1 https://github.com/vllm-project/vllm.git vllm-v0.10.1
+        cd vllm-v0.10.1
+        pip download "vllm==0.10.1" --no-deps -d /tmp
+        export VLLM_PRECOMPILED_WHEEL_LOCATION=/tmp/vllm-0.10.1-cp38-abi3-manylinux1_x86_64.whl
+        uv pip install --editable .
+        git apply "$SCRIPT_DIR/vllm-v0.10.1.patch"
+        rm -rf /tmp/vllm-0.10.1-cp38-abi3-manylinux1_x86_64.whl
+        deactivate
+        popd
+    else
+        echo "vllm already exists"
+    fi
+    echo "vllm setup complete"
+}
+
+setup_diffusion() {
+    echo "Setting up diffusion..."
+    source $GVM_DIR/.venv/bin/activate
+    uv pip install diffusers sentencepiece transformers accelerate protobuf
+    deactivate
+    echo "diffusion setup complete"
+}
+
+setup_llamafactory() {
+    echo "Setting up llama factory..."
+    if [ ! -d $GVM_DIR/app/LLaMA-Factory ]; then
+        source $GVM_DIR/.venv/bin/activate
+        pushd $GVM_DIR/app/
+        git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
+        cd LLaMA-Factory
+        uv pip install -e ".[torch,metrics]" --no-build-isolation
+        deactivate
+        popd
+    else
+        echo "llama-factory already exists"
+    fi
+    echo "llama-factory setup complete"
+}
+
 check_and_install_uv
 setup_uv_venv
+
+setup_vllm
+setup_diffusion
+setup_llamafactory
 
 echo "Setup complete"
